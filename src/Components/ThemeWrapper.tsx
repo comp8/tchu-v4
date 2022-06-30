7cda03f84bd1157018cfe2ccebd8fc909e1fb82a
MK <comp812@gmail.com>
2022-06-30 17:31:11 +0900

    root fontsize

--- src/Components/ThemeWrapper.tsx
+++ src/Components/ThemeWrapper.tsx
@@ -17,15 +17,12 @@ function useTheme() {
 
 interface Props {
   className?: string;
-  rootFontSize?: number;
   children?: React.ReactElement;
 }
 export default function ThemeWrapper(props: Props) {
 
   // TODO:
   const [theme, setTheme] = useTheme();
-
-  document.documentElement.style.setProperty('font-size', `${props.rootFontSize || Config.style.defaultRootFontSize}px`);
 
   return (
     <div className={props.className}>{props.children || null}</div>
