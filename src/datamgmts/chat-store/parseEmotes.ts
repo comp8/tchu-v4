ff6dc15d3b8b445a06459bb1bd55a341c3b8abb7
MK <comp812@gmail.com>
2022-07-08 00:17:11 +0900

    wip

--- src/datamgmts/chat-store/parseEmotes.ts
+++ src/datamgmts/chat-store/parseEmotes.ts
@@ -2,19 +2,25 @@ import tmi from 'tmi.js';
 import ChatStoreTypes from './types';
 
 export function parseEmotes(message: string, emoteObj?: tmi.CommonUserstate['emotes']): ChatStoreTypes.ChatMessage {
-  // if (emoteObj) {
-  //   try {
-  //     Object.keys(emoteObj).map(id => {
-  //       emoteObj[id].map(item => {
-  //         const [start, end] = item.split('-');
-  //         return 
-  //       })
-  //     })
-  //   }
-  //   catch (err) {
-  //     console.error('emotes parsing error', emoteObj);
-  //   }
-  // }
+  if (!emoteObj) {
+    return [message];
+  }
 
-  return [message];
+  const result: ChatStoreTypes.ChatMessage = [];
+
+  Object.keys(emoteObj)
+    .map(id => emoteObj[id].map(item => {
+      const [start, end] = item.split('-').map(e => parseInt(e));
+      return { start, end, id };
+    }))
+    .flat()
+    .sort((a, b) => a.start - b.end)
+    .reduce((prevIndex, emote) => {
+      if (prevIndex != emote.start)
+        result.push(message.slice(prevIndex, emote.start));
+      result.push({ id: emote.id });
+      return emote.end + 1;
+    }, 0);
+
+  return result;
 }
